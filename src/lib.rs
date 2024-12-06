63c61ccaa1aadd7de3d8c10d329e6046514074fa
N/A <N/A>
2024-12-05 19:20:40 -0500

    class factory outline

--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,31 @@
-use std::cell::RefCell;
-use regex::Regex;
-use std::sync::LazyLock;
-use std::ffi::c_void;
+use windows::{core::*, Win32::System::Com::*};
 
-use windows::{core::*, Win32::System::Com::*, Win32::System::Registry::*};
-use windows::Win32::UI::Shell::PropertiesSystem::*;
+#[implement(IClassFactory)]
+struct ClassFactory;
 
-
-#[implement(IInitializeWithStream,IPropertyStore)]
-pub struct PropertyHandler {
-   orig_ps: RefCell<Option<IPropertyStore>>,
-}
-
-
-static EXTENSION_REGEX: LazyLock<Regex> = 
-    LazyLock::new(|| Regex::new(r#"\.[^\.]*+$"#).unwrap());
-
-fn get_file_type(file_name: &str) -> &str {
-    let ext = EXTENSION_REGEX.find(file_name).unwrap();
-    ext.as_str()
-}
-
-
-#[allow(non_snake_case, unused_variables)]
-impl IInitializeWithStream_Impl for PropertyHandler_Impl {
-    fn Initialize(&self, pstream: Option<&IStream>, grfmode: u32) -> Result<()> {
-        
-        let _ = unsafe{CoIncrementMTAUsage()};
-        
-        //Identifying file type
-        let pstatstg: &mut STATSTG = &mut Default::default();
-        let grfStatFlag = STATFLAG(0);
-
-        let pstream = pstream.unwrap();
-        let ext = unsafe {
-            pstream.Stat(pstatstg, grfStatFlag)?;
-            let file_name = pstatstg.pwcsName.to_string()?;
- 
-            get_file_type(&file_name).to_owned()
-        };
-
-
-        //Getting IPropertyStore using the file type's original handler
-        let mut orig_key = String::with_capacity(85);
-        orig_key.push_str(r#"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PropertySystem\\PropertyHandlers\\"#);
-        orig_key.push_str(&ext);
-
-        let inter: Vec<u16> = orig_key.encode_utf16().collect();
-        let lpSubKey: PCWSTR = PCWSTR::from_raw(inter.as_ptr());
-
-        let mut phkResult: HKEY = Default::default();
-        
-        let orig_ps: IPropertyStore = unsafe {
-            RegOpenKeyExW(HKEY_LOCAL_MACHINE,lpSubKey,0,KEY_QUERY_VALUE,&mut phkResult).ok()?;
-
-            let mut buffer = [0u16; 64];
-            let mut size: u32 = (64 * std::mem::size_of_val(&buffer[0])) as u32;
-            RegGetValueW(phkResult,None,None,RRF_RT_REG_SZ,None,
-                Some(buffer.as_mut_ptr() as *mut c_void),Some(&mut size)).ok()?;
-            
-            RegCloseKey(phkResult).ok()?;
-            let orig_clsid: GUID = CLSIDFromString(PWSTR::from_raw(buffer.as_mut_ptr()))?;
-            
-            let orig_init: IInitializeWithStream = CoCreateInstance(&orig_clsid,None,CLSCTX_ALL)?;
-            orig_init.Initialize(pstream,0x00000002)?;
-            orig_init.cast()?
-        };
-
-        *self.orig_ps.borrow_mut() = Some(orig_ps);
-    
-        Ok(())
-    }
-}
-
-#[allow(non_snake_case, unused_variables)]
-impl IPropertyStore_Impl for PropertyHandler_Impl {
-    fn GetCount(&self) -> Result<u32> {
-        let binding = self.orig_ps.borrow();
-        let ps = binding.as_ref().unwrap();
-        unsafe{ps.GetCount()}
-    }
-
-    fn GetAt(&self, iprop: u32, pkey: *mut PROPERTYKEY) -> Result<()> {
-        let binding = self.orig_ps.borrow();
-        let ps = binding.as_ref().unwrap();
-        unsafe{ps.GetAt(iprop,pkey)}
-    }
-
-    fn GetValue(&self, key: *const PROPERTYKEY) -> Result<PROPVARIANT> {
-        let binding = self.orig_ps.borrow();
-        let ps = binding.as_ref().unwrap();
-        unsafe{ps.GetValue(key)}
-    }
-
-    fn SetValue(
+impl IClassFactory_Impl for ClassFactory_Impl {
+    fn CreateInstance(
         &self,
-        key: *const PROPERTYKEY,
-        propvar: *const PROPVARIANT,
+        punkouter: Option<&IUnknown>,
+        riid: *const GUID,
+        ppvobject: *mut *mut c_void,
     ) -> Result<()> {
+        
         Ok(())
     }
 
-    fn Commit(&self) -> Result<()> {
-        
+    fn LockServer(&self, flock: BOOL) -> Result<()> {
+
         Ok(())
     }
 }
 
-#[cfg(test)]
-mod tests;
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+pub unsafe extern "system" fn DllGetClassObject (
+    rclsid: *const GUID,
+    riid: *const GUID,
+    pout: *mut *mut core::ffi::c_void,
+) -> HRESULT {
+    
+}
--- src/tests.rs
+++ src/tests.rs
@@ -7,18 +7,19 @@ use windows::Win32::UI::Shell::{SHCreateStreamOnFileEx,PropertiesSystem::IProper
 #[allow(non_snake_case, unused_variables)]
 fn init_test() -> Result<()> {
     let stream: &IStream = unsafe {
-        let img_path = r"C:\Users\nobody\Pictures\arc\arc38\4cf20cb6d400ca27139246249567aa7f.png";
+        let img_path = r"C:\Users\nobody\Pictures\arc\arc38\4a640c75ee8439375004ccb05ae123df.jpg";
 
         let middle: Vec<u16> = img_path.encode_utf16().collect();
-
-        //println!("{:?}", middle);
-
         let pszFile: PCWSTR = PCWSTR::from_raw(middle.as_ptr());
 
         &SHCreateStreamOnFileEx(pszFile,0,0,BOOL(0),None)?
     };
 
-    let ph: IInitializeWithStream = PropertyHandler{orig_ps: RefCell::new(None)}.into();
+    let ipf: IPersistFile = stream.cast()?;
+    let fpath = unsafe{ipf.GetCurFile()?.to_string()?};
+    println!("{}", fpath);
+
+    let ph: IInitializeWithStream = PropertyHandler{orig_ps: Default::default()}.into();
     unsafe {ph.Initialize(Some(stream),0)?;}
 
     let store: IPropertyStore = ph.cast()?;
